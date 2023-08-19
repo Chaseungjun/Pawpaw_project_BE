df1aa881be7dae56efc5f66728ce64b5504039d1
Chaseoungjun <chas369@naver.com>
2023-08-19 15:47:48 +0900

    #20 게시글을 조회할 때, 등록된 이미지를 같이 가지고 오기 위한 findByIdWithImages 메서드 생성

--- domain/domain-rdb/src/main/java/kr/co/pawpaw/domainrdb/board/repository/BoardRepository.java
+++ domain/domain-rdb/src/main/java/kr/co/pawpaw/domainrdb/board/repository/BoardRepository.java
@@ -1,7 +1,16 @@
 package kr.co.pawpaw.domainrdb.board.repository;
 
 import kr.co.pawpaw.domainrdb.board.Board;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    @EntityGraph(attributePaths = {"imgSet"}) // board와  Lazy 연관관계를 맺고 있는 img도 한번에 가져오게함
+    @Query("select b from Board b where b.id =:id")
+    Optional<Board> findByIdWithImages(@Param("id") Long id);
 }
