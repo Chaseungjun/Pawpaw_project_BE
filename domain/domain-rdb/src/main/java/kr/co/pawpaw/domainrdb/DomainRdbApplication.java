ba279520e674e24b64c421b66de9e24895bc1d36
Chaseoungjun <chas369@naver.com>
2023-08-19 16:37:59 +0900

    #20 테스트 코드 작성 및 적용을 위한 DomainRdbApplication 클래스 추가, 썸네일 생성을 위한 의존성을 추가

---
+++ domain/domain-rdb/src/main/java/kr/co/pawpaw/domainrdb/DomainRdbApplication.java
@@ -0,0 +1,7 @@
+package kr.co.pawpaw.domainrdb;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "kr.co.pawpaw.domainrdb.*")
+public class DomainRdbApplication {
+}
